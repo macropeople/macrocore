5ca9e944122a2d35f24e6576080708d4cd81ba45
Allan Bowe <>
2020-01-26 02:22:11 +0100

    feat: new build script

---
+++ build.py
@@ -0,0 +1,37 @@
+# Concatenate all macros into a single file
+
+import os
+
+OUTFILE='./macrocore.sas'
+header="""
+/**
+  @file
+  @brief Auto-generated file
+  @details
+    This file contains all the macros in a single file - which means it can be
+    'included' in SAS with just 2 lines of code:
+
+      filename mc url
+        "https://raw.githubusercontent.com/macropeople/macrocore/master/compileall.sas";
+      %inc mc;
+
+    The `build.py` file in the https://github.com/macropeople/macrocore repo
+    is used to create this file.
+
+  @author Allan Bowe
+**/
+"""
+f = open("test.txt", "w")             # r / r+ / rb / rb+ / w / wb
+f.write(header)
+folders=['base','meta','xcmd','viya']
+for folder in folders:
+    filenames=os.listdir('./' + folder) 
+    with open('compile' + folder + '.sas', 'w') as outfile:
+        for fname in filenames:
+            with open('./' + folder + '/' + fname) as infile:
+                outfile.write(infile.read())
+    with open('compile' + folder + '.sas','r') as c:
+        f.write(c.read())
+f.close()
+
+
